5a1cfe8412bfd6f65c3dcd5f5f6b2210253cb278
YASUI Kentarow <yasuikentarow@gmail.com>
2020-02-29 08:10:58 +0900

    _input => input

--- src/Grammer.hpp
+++ src/Grammer.hpp
@@ -3,19 +3,19 @@ class Grammer : public lilyan::Parser {
  public:
   bool rule_rules(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 1> _args;
       if(isMatch(lilyan::Repeat::ZeroAny, [this](std::any* r) { return rule_rule(r); }, &_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("onRules", [this, _args]() { return onRules(eval(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_rule(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 2> _args;
       if(getToken(std::regex(R"(\w+)"), &_args.at(0)) &&
@@ -24,25 +24,25 @@ class Grammer : public lilyan::Parser {
          getToken(std::string(";"))) {
         setMatch(match, std::make_shared<lilyan::Action>("onRule", [this, _args]() { return onRule(std::any_cast<std::smatch>(_args.at(0)), eval(_args.at(1))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_semantics(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 1> _args;
       if(isMatch(lilyan::Repeat::OneAny, [this](std::any* r) { return rule_semantic(r); }, &_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("onSemantics", [this, _args]() { return onSemantics(eval(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_semantic(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 2> _args;
       if(rule_tokens(&_args.at(0)) &&
@@ -50,82 +50,82 @@ class Grammer : public lilyan::Parser {
          rule_action(&_args.at(1))) {
         setMatch(match, std::make_shared<lilyan::Action>("onSemantic", [this, _args]() { return onSemantic(eval(_args.at(0)), eval(_args.at(1))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_tokens(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 1> _args;
       if(isMatch(lilyan::Repeat::OneAny, [this](std::any* r) { return rule_token(r); }, &_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("onTokens", [this, _args]() { return onTokens(eval(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_token(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 2> _args;
       if(getToken(std::regex(R"(\w+)"), &_args.at(0)) &&
          isMatch(lilyan::Repeat::ZeroOne, [this](std::any* r) { return rule_token_r(r); }, &_args.at(1))) {
         setMatch(match, std::make_shared<lilyan::Action>("tokenRule", [this, _args]() { return tokenRule(std::any_cast<std::smatch>(_args.at(0)), eval(_args.at(1))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     {
       std::array<std::any, 1> _args;
       if(getToken(std::string("''"), &_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("tokenString", [this, _args]() { return tokenString(std::any_cast<std::string>(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     {
       std::array<std::any, 1> _args;
       if(getToken(std::string("\"\""), &_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("tokenString", [this, _args]() { return tokenString(std::any_cast<std::string>(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     {
       std::array<std::any, 1> _args;
       if(getToken(std::string("//"), &_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("tokenRegexp", [this, _args]() { return tokenRegexp(std::any_cast<std::string>(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_token_r(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       if(getToken(std::string("?"))) {
         setMatch(match, lilyan::Repeat::ZeroOne);
       }
-      getInput() = _input;
+      getInput() = input;
     }
     {
       if(getToken(std::string("*"))) {
         setMatch(match, lilyan::Repeat::ZeroAny);
       }
-      getInput() = _input;
+      getInput() = input;
     }
     {
       if(getToken(std::string("+"))) {
         setMatch(match, lilyan::Repeat::OneAny);
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_action(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 2> _args;
       if(getToken(std::regex(R"(\w+)"), &_args.at(0)) &&
@@ -134,7 +134,7 @@ class Grammer : public lilyan::Parser {
          getToken(std::string(")"))) {
         setMatch(match, std::make_shared<lilyan::Action>("onActionRule", [this, _args]() { return onActionRule(std::any_cast<std::smatch>(_args.at(0)), eval(_args.at(1))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     {
       std::array<std::any, 1> _args;
@@ -143,52 +143,52 @@ class Grammer : public lilyan::Parser {
          getToken(std::string(">"))) {
         setMatch(match, std::make_shared<lilyan::Action>("onActionConst", [this, _args]() { return onActionConst(std::any_cast<std::smatch>(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     {
       std::array<std::any, 1> _args;
       if(rule_arg(&_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("onActionArg", [this, _args]() { return onActionArg(eval(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_args(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 2> _args;
       if(rule_arg(&_args.at(0)) &&
          isMatch(lilyan::Repeat::ZeroAny, [this](std::any* r) { return rule_args_r(r); }, &_args.at(1))) {
         setMatch(match, std::make_shared<lilyan::Action>("onArgs", [this, _args]() { return onArgs(eval(_args.at(0)), eval(_args.at(1))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_args_r(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::any _arg;
       if(getToken(std::string(",")) &&
          rule_arg(&_arg)) {
         setMatch(match, _arg);
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
   bool rule_arg(std::any* result = nullptr) {
     Match match;
-    lilyan::Input _input(getInput());
+    lilyan::Input input(getInput());
     {
       std::array<std::any, 1> _args;
       if(getToken(std::regex(R"(\$(\d+))"), &_args.at(0))) {
         setMatch(match, std::make_shared<lilyan::Action>("onArg", [this, _args]() { return onArg(std::any_cast<std::smatch>(_args.at(0))); }));
       }
-      getInput() = _input;
+      getInput() = input;
     }
     return applyMatch(match, result);
   }
